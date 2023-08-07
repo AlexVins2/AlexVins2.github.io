098b72c8f99684fce0773f225a39270168b0f113
AlexVins2 <140976655+AlexVins2@users.noreply.github.com>
2023-08-06 21:26:06 -0600

    Add files via upload

---
+++ js/interacción.js
@@ -0,0 +1,67 @@
+function alerta(){
+    var pregunta = confirm("¿Estas a punto de comprar Vino Apertura?")
+        if (pregunta){
+            alert("Te envío allí rápidamente")
+            window.location = "https://vinedosazteca.com/shop";
+        }
+        else{
+            alert("Quizás en otro momento")
+        }
+
+}
+
+function alerta2(){
+    var pregunta = confirm("¿Estas a punto de comprar Vino Pretexto?")
+    if (pregunta){
+        alert("Te envío allí rápidamente")
+        window.location = "https://vinedosazteca.com/shop";
+    }
+    else{
+        alert("Quizás en otro momento")
+    }
+
+}
+
+function alerta3(){
+    var pregunta = confirm("¿Estas a punto de comprar Vino Colibrí?")
+    if (pregunta){
+        alert("Te envío allí rápidamente")
+        window.location = "https://vinedosazteca.com/shop";
+    }
+    else{
+        alert("Quizás en otro momento")
+    }
+}
+
+function alerta4(){
+    var pregunta = confirm("¿Estas a punto de comprar Vino Retinto?")
+    if (pregunta){
+        alert("Te envío allí rápidamente")
+        window.location = "https://vinedosazteca.com/shop";
+    }
+    else{
+        alert("Quizás en otro momento")
+    }
+}
+function alerta5(){
+    var pregunta = confirm("¿Estas a punto de comprar Vino Rosillo?")
+    if (pregunta){
+        alert("Te envío allí rápidamente")
+        window.location = "https://vinedosazteca.com/shop";
+    }
+    else{
+        alert("Quizás en otro momento")
+    }
+}
+
+function alerta6(){
+    var pregunta = confirm("¿Estas a punto de comprar Vino Tordillo?")
+    if (pregunta){
+        alert("Te envío allí rápidamente")
+        window.location = "https://vinedosazteca.com/shop";
+    }
+    else{
+        alert("Quizás en otro momento")
+    }
+
+}
